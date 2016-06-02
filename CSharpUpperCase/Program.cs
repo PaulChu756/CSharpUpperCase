09f1159d0d6ccbdfb82a5766ebd74eb17fae7281
Paul Chu <lordbabypanda@gmail.com>
2016-06-02 18:00:45 -0500

    UpperCase C#

---
+++ CSharpUpperCase/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpUpperCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "the quick brown fox";
+            char[] charArray;
+            char currentLetter;
+
+            charArray = s.ToCharArray();
+
+            for (int i = 0; i < charArray.Length; ++i)
+            {
+                currentLetter = charArray[i];
+                if(char.IsLower(currentLetter))
+                {
+                    charArray[i] = char.ToUpper(currentLetter);
+                }
+            }
+            Console.Write(charArray);
+            Console.Read();
+        }
+    }
+}
